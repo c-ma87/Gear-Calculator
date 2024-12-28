34a48885bfb721cf29fb75cfdca211bffbabc615
cma32 <cma32@students.cs.ubc.ca>
2024-12-27 23:27:37 -0800

    Refactored GearCalculator and StatBoost

--- src/main/model/GearCalculator.java
+++ src/main/model/GearCalculator.java
@@ -1,5 +1,36 @@
 package main.model;
 
+// represents a calculator to calculate total stat boosts from gear
 public class GearCalculator {
 
+    // EFFECTS: constructs a gear calculator with stat calculation formulas
+    public GearCalculator() {
+        // TODO
+    }
+
+    // EFFECTS: returns multiplier as determined by the piecewise defined formula:
+    // if boost <= 235, multiplier = boost/100 + 1
+    // if boost > 235,
+    // multiplier = 3 - 3 / (e^((1/500) * boost + ln(128) - 237 * (3.008/189))) + 1
+    public double calculateDamageMultiplier(int damage) {
+        if (damage <= 235) {
+            return (double) damage / 100.0 + 1.0;
+        } else {
+            return 3.0
+                    - 3.0 / (Math.exp((1.0 / 500.0) * (double) damage + Math.log(128.0) - 237.0 * (3.008 / 189.0)))
+                    + 1.0;
+        }
+    }
+
+    // EFFECTS: returns fraction (resist) multiplier determined by the formula:
+    // multiplier = 1 - boost/100
+    // if multiplier < 0, returns 0.0
+    public double calculateResistMultiplier(int resist) {
+        double multiplier = 1.0 - (double) resist / 100;
+        if (multiplier < 0) {
+            return 0.0;
+        } else {
+            return multiplier;
+        }
+    }
 }
--- src/test/GearCalculatorTest.java
+++ src/test/GearCalculatorTest.java
@@ -1,5 +1,50 @@
 package test;
 
-public class GearCalculatorTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.model.GearCalculator;
+
+public class GearCalculatorTest {
+    private GearCalculator testGearCalculator;
+
+    @BeforeEach
+    void runBefore() {
+        testGearCalculator = new GearCalculator();
+    }
+
+    @Test
+    void testCalculateDamageMultiplierNoBoost() {
+        assertEquals(1.0, testGearCalculator.calculateDamageMultiplier(0));
+    }
+
+    @Test
+    void testCalculateDamageMultiplierBelowIntersect() {
+        assertEquals(3.30, testGearCalculator.calculateDamageMultiplier(230));
+    }
+
+    @Test
+    void testCalculateDamageMultiplierAtIntersect() {
+        assertEquals(3.35, testGearCalculator.calculateDamageMultiplier(235));
+    }
+
+    @Test
+    void testCalculateDamageMultiplierJustAboveIntersect() {
+        assertEquals(3.36, testGearCalculator.calculateDamageMultiplier(236), 0.05);
+    }
+
+    @Test
+    void testCalculateDamageMultiplierMuchAboveIntersect() {
+        assertEquals(3.39, testGearCalculator.calculateDamageMultiplier(260), 0.05);
+    }
+
+    @Test
+    void testCalculateResistMultiplier() {
+        assertEquals(1.0, testGearCalculator.calculateResistMultiplier(0));
+        assertEquals(0.90, testGearCalculator.calculateResistMultiplier(10));
+        assertEquals(0.0, testGearCalculator.calculateResistMultiplier(100));
+        assertEquals(0.0, testGearCalculator.calculateResistMultiplier(110));
+    }
 }
