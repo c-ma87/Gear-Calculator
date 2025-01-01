3a0362282de263e2975402d98bec7dd99f95f517
cma32 <cma32@students.cs.ubc.ca>
2024-12-31 20:55:48 -0800

    Added tests for SetBonus

--- src/main/model/setBonus/SetBonus.java
+++ src/main/model/setBonus/SetBonus.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 // represents a gear set bonus with tiers of set bonus stat boosts
+// listed in chronological order based on list position
+// ex. tier 1 is the first item in tiers, and is fulfilled if the condition
+// for tier 1 is met
 public class SetBonus {
     private String name;
     private List<Tier> tiers;
+    private List<Integer> tierConditions;
 
     // EFFECTS: constructs a set bonus with a name and no tiers
     public SetBonus() {
@@ -26,7 +30,26 @@ public class SetBonus {
         // TODO
     }
 
+    // REQUIRES: condition >= 0
+    // MODIFIES: this
+    // EFFECTS: adds condition to tierConditions
+    public void addTierCondition(int condition) {
+        // TODO
+    }
+
+    // REQUIRES: conditions.size() > 0 && 0 <= condition <= conditions.size() - 1
+    // MODIFIES: this
+    // EFFECTS: removes condition with index tierNum from conditions
+    public void removeTierCondition(int tierNum) {
+        // TODO
+    }
+
     public List<Tier> getTiers() {
+        // TODO
+        return new ArrayList<>();
+    }
+
+    public List<Integer> getTierConditions() {
         // TODO
         return new ArrayList<>();
     }
---
+++ src/test/SetBonusTest.java
@@ -0,0 +1,116 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.model.StatBoost;
+import main.model.setBonus.SetBonus;
+import main.model.setBonus.Tier;
+
+public class SetBonusTest {
+    private SetBonus testSetBonus;
+    private Tier tier1;
+    private Tier tier2;
+    private Tier tier3;
+
+    @BeforeEach
+    void runBefore() {
+        testSetBonus = new SetBonus();
+        tier1 = new Tier();
+        tier2 = new Tier();
+        tier3 = new Tier();
+    }
+    
+    @Test
+    void testConstructor() {
+        List<Tier> tiers = testSetBonus.getTiers();
+        assertEquals(0, tiers.size());
+    
+        List<Integer> conditions = testSetBonus.getTierConditions();
+        assertEquals(0, conditions.size());
+    }
+
+    @Test
+    void testAddTier() {
+        List<Tier> tiers = testSetBonus.getTiers();
+
+        tier1.addStatBoost(new StatBoost(3, "life", "accuracy"));
+        testSetBonus.addTier(tier1);
+
+        assertEquals(1, tiers.size());
+        assertEquals(3, tiers.get(0).getStatBoosts().get(0).getBoost());
+        assertEquals("life", tiers.get(0).getStatBoosts().get(0).getSchool());
+        assertEquals("accuracy", tiers.get(0).getStatBoosts().get(0).getName());
+
+        testSetBonus.addTier(tier2);
+        testSetBonus.addTier(tier3);
+        assertEquals(3, tiers.size());
+        assertEquals(tier1, tiers.get(0));
+        assertEquals(tier2, tiers.get(1));
+        assertEquals(tier3, tiers.get(2));
+    }
+
+    @Test
+    void testRemoveTier() {
+        List<Tier> tiers = testSetBonus.getTiers();
+
+        testSetBonus.addTier(tier1);
+        testSetBonus.addTier(tier2);
+        testSetBonus.addTier(tier3);
+        assertEquals(3, tiers.size());
+
+        testSetBonus.removeTier(tier2);
+        assertEquals(2, tiers.size());
+        assertEquals(tier1, tiers.get(0));
+        assertEquals(tier3, tiers.get(1));
+
+        testSetBonus.removeTier(tier1);
+        assertEquals(1, tiers.size());
+        assertEquals(tier2, tiers.get(0));
+
+        testSetBonus.removeTier(tier2);
+        assertEquals(0, tiers.size());
+    }
+
+    @Test
+    void testAddTierCondition() {
+        List<Integer> conditions = testSetBonus.getTierConditions();
+
+        testSetBonus.addTierCondition(2);
+        assertEquals(1, conditions.size());
+        assertEquals(2, conditions.get(0));
+
+        testSetBonus.addTierCondition(5);
+        testSetBonus.addTierCondition(8);
+        assertEquals(3, conditions.size());
+        assertEquals(2, conditions.get(0));
+        assertEquals(5, conditions.get(1));
+        assertEquals(8, conditions.get(2));
+    }
+
+    @Test
+    void testRemoveTierCondition() {
+        List<Integer> conditions = testSetBonus.getTierConditions();
+
+        testSetBonus.addTierCondition(8);
+        testSetBonus.addTierCondition(10);
+        testSetBonus.addTierCondition(1);
+        assertEquals(3, conditions.size());
+
+        testSetBonus.removeTierCondition(2);
+        assertEquals(2, conditions.size());
+        assertEquals(8, conditions.get(0));
+        assertEquals(10, conditions.get(1));
+
+        testSetBonus.removeTierCondition(1);
+        assertEquals(1, conditions.size());
+        assertEquals(10, conditions.get(0));
+
+        testSetBonus.removeTierCondition(0);
+        assertEquals(0, conditions.size());
+    }
+}
