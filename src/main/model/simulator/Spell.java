75eacc79d6a093ed4a111395f5bf9004ced05c8f
cma32 <cma32@students.cs.ubc.ca>
2025-05-01 11:45:28 -0700

    Added specification for Spell

---
+++ src/main/model/simulator/Spell.java
@@ -0,0 +1,54 @@
+package main.model.simulator;
+
+public class Spell {
+    int damage;
+    int healing;
+    int shield;
+    int ward;
+    boolean aoe;
+
+    // EFFECTS: creates a spell with a name and stats
+    public Spell() {
+        // TODO
+    }
+
+    public void setDamage() {
+        // TODO
+    }
+
+    public void setHealing() {
+        // TODO
+    }
+
+    public void setShield() {
+        // TODO
+    }
+
+    public void setWard() {
+        // TODO
+    }
+
+    public void setAoe() {
+        // TODO
+    }
+
+    public void getDamage() {
+        // TODO
+    }
+
+    public void getHealing() {
+        // TODO
+    }
+
+    public void getShield() {
+        // TODO
+    }
+
+    public void getWard() {
+        // TODO
+    }
+
+    public void getAoe() {
+        // TODO
+    }
+}
