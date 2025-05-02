ec3d2e0937004257c84f80524214e5be6369fa16
cma32 <cma32@students.cs.ubc.ca>
2025-05-02 11:38:07 -0700

    Added specification for PlayerGear

---
+++ src/main/model/simulator/player/PlayerGear.java
@@ -0,0 +1,102 @@
+package main.model.simulator.player;
+
+import main.model.GearItem;
+
+public class PlayerGear {
+    private GearItem hat;
+    private GearItem robe;
+    private GearItem boots;
+    private GearItem wand;
+    private GearItem athame;
+    private GearItem amulet;
+    private GearItem ring;
+    private GearItem deck;
+    private GearItem pet;
+    private GearItem mount;
+
+    public PlayerGear() {
+        // TODO
+    }
+
+    public void setHat() {
+        // TODO
+    }
+
+    public void setRobe() {
+        // TODO
+    }
+    public void setBoots() {
+        // TODO
+    }
+    public void setWand() {
+        // TODO
+    }
+    public void setAthame() {
+        // TODO
+    }
+    public void setAmulet() {
+        // TODO
+    }
+    public void setRing() {
+        // TODO
+    }
+    public void setDeck() {
+        // TODO
+    }
+    public void setPet() {
+        // TODO
+    }
+    public void setMount() {
+        // TODO
+    }
+
+    public GearItem getHat() {
+        // TODO
+        return null;
+    }
+
+    public GearItem getRobe() {
+        // TODO
+        return null;
+    }
+
+    public GearItem getBoots() {
+        // TODO
+        return null;
+    }
+
+    public GearItem getWand() {
+        // TODO
+        return null;
+    }
+
+    public GearItem getAthame() {
+        // TODO
+        return null;
+    }
+
+    public GearItem getAmulet() {
+        // TODO
+        return null;
+    }
+
+    public GearItem getRing() {
+        // TODO
+        return null;
+    }
+
+    public GearItem getDeck() {
+        // TODO
+        return null;
+    }
+
+    public GearItem getPet() {
+        // TODO
+        return null;
+    }
+
+    public GearItem getMount() {
+        // TODO
+        return null;
+    }
+}
