1422673f33e2999d70d9d09225668240e294cc79
cma32 <cma32@students.cs.ubc.ca>
2025-05-02 13:48:31 -0700

    Added specification for PlayerStats

---
+++ src/main/model/simulator/player/PlayerStats.java
@@ -0,0 +1,98 @@
+package main.model.simulator.player;
+
+public class PlayerStats {
+    private int level;
+    private int health;
+    private int mana;
+    private int damage;
+    private int resist;
+    private int critical;
+    private int block;
+    private int accuracy;
+    private int powerPip;
+    private int pierce;
+
+    public PlayerStats() {
+        // TODO
+    }
+
+    public void setLevel(int level) {
+        // TODO
+    }
+
+    public void setHealth(int health) {
+        // TODO
+    }
+
+    public void setMana(int mana) {
+        // TODO
+    }
+
+    public void setDamage(int damage) {
+        // TODO
+    }
+
+    public void setResist(int resist) {
+        // TODO
+    }
+
+    public void setCritical(int critical) {
+        // TODO
+    }
+
+    public void setBlock(int block) {
+        // TODO
+    }
+
+    public void setAccuracy(int accuracy) {
+        // TODO
+    }
+
+    public void setPowerPip(int powerPip) {
+        // TODO
+    }
+
+    public void setPierce(int pierce) {
+        // TODO
+    }
+
+    public int getLevel() {
+        return 0;
+    }
+
+    public int getHealth() {
+        return 0;
+    }
+
+    public int getMana() {
+        return 0;
+    }
+
+    public int getDamage() {
+        return 0;
+    }
+
+    public int getResist() {
+        return 0;
+    }
+
+    public int getCritical() {
+        return 0;
+    }
+
+    public int getBlock() {
+        return 0;
+    }
+
+    public int getAccuracy() {
+        return 0;
+    }
+
+    public int getPowerPip() {
+        return 0;
+    }
+
+    public int getPierce() {
+        return 0;
+    }
+}
