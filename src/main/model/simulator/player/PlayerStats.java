09519797c3597a1c451cf21957446727396a5268
cma32 <cma32@students.cs.ubc.ca>
2025-05-02 14:26:14 -0700

    Updated PlayerStats to use HashMap for multi-school stats

--- src/main/model/simulator/player/PlayerStats.java
+++ src/main/model/simulator/player/PlayerStats.java
@@ -1,18 +1,21 @@
 package main.model.simulator.player;
 
+import java.util.HashMap;
+
 public class PlayerStats {
     private int level;
     private int health;
     private int mana;
-    private int damage;
-    private int resist;
-    private int critical;
-    private int block;
-    private int accuracy;
     private int powerPip;
-    private int pierce;
+    private HashMap<String, Integer> damage;
+    private HashMap<String, Integer> resist;
+    private HashMap<String, Integer> critical;
+    private HashMap<String, Integer> block;
+    private HashMap<String, Integer> accuracy;
+    private HashMap<String, Integer> pierce;
 
-    public PlayerStats() {
+    public PlayerStats(int level, int health, int mana, int damage, int resist, int critical, int block, int accuracy,
+            int powerPip, int pierce) {
         // TODO
     }
 
@@ -28,31 +31,49 @@ public class PlayerStats {
         // TODO
     }
 
-    public void setDamage(int damage) {
-        // TODO
-    }
-
-    public void setResist(int resist) {
-        // TODO
-    }
-
-    public void setCritical(int critical) {
-        // TODO
-    }
-
-    public void setBlock(int block) {
-        // TODO
-    }
-
-    public void setAccuracy(int accuracy) {
-        // TODO
-    }
-
     public void setPowerPip(int powerPip) {
         // TODO
     }
 
-    public void setPierce(int pierce) {
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: sets damage stat with associated school to damage
+    public void setDamage(int damage, String school) {
+        // TODO
+    }
+
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: sets resist stat with associated school to resist
+    public void setResist(int resist, String school) {
+        // TODO
+    }
+
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: sets critical stat with associated school to critical
+    public void setCritical(int critical, String school) {
+        // TODO
+    }
+
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: sets block stat with associated school to block
+    public void setBlock(int block, String school) {
+        // TODO
+    }
+
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: sets accuracy stat with associated school to accuracy
+    public void setAccuracy(int accuracy, String school) {
+        // TODO
+    }
+
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: sets pierce stat with associated school to pierce
+    public void setPierce(int pierce, String school) {
         // TODO
     }
 
@@ -68,31 +89,55 @@ public class PlayerStats {
         return 0;
     }
 
-    public int getDamage() {
-        return 0;
-    }
-
-    public int getResist() {
-        return 0;
-    }
-
-    public int getCritical() {
-        return 0;
-    }
-
-    public int getBlock() {
-        return 0;
-    }
-
-    public int getAccuracy() {
-        return 0;
-    }
-
     public int getPowerPip() {
         return 0;
     }
 
-    public int getPierce() {
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: if damage stat with associated school exists, returns the stat value
+    // otherwise returns 0
+    public int getDamage(String school) {
+        return 0;
+    }
+
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: if resist stat with associated school exists, returns the stat value
+    // otherwise returns 0
+    public int getResist(String school) {
+        return 0;
+    }
+
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: if critical stat with associated school exists, returns the stat value
+    // otherwise returns 0
+    public int getCritical(String school) {
+        return 0;
+    }
+
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: if block stat with associated school exists, returns the stat value
+    // otherwise returns 0
+    public int getBlock(String school) {
+        return 0;
+    }
+
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: if accuracy stat with associated school exists, returns the stat value
+    // otherwise returns 0
+    public int getAccuracy(String school) {
+        return 0;
+    }
+
+    // REQUIRES: school is one of "fire", "ice", "myth", "storm", "life", "death",
+    // or "balance"
+    // EFFECTS: if pierce stat with associated school exists, returns the stat value
+    // otherwise returns 0
+    public int getPierce(String school) {
         return 0;
     }
 }
